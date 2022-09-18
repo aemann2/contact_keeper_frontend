9580c243e21ce5f8fa7446c584c836e34cac6faf
aemann2 <aemann2@gmail.com>
2022-09-18 10:00:04 -0700

    initial setup to retrieve backend data

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,9 +1,25 @@
-import React, {useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Index = () => {
-	return (
-		<div>Index</div>
-	)
+    const [post, setPost] = useState(null)
+    const URL = process.env.REACT_APP_BASE_URL + 'api/contacts/'
+
+    useEffect(() => {
+        axios.get(URL).then((response) => {
+            setPost(response.data)
+        })
+    }, [URL])
+
+    if (!post) return null
+
+    return (
+        <div>
+            {post.map((post) => {
+                return <h1>{post.name}</h1>
+            })}
+        </div>
+    )
 }
 
 export default Index
